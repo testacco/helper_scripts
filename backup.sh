17fdbfc480394e39233c4f767caaf479d83e8150
root <root@localhost.localdomain>
2017-09-17 23:28:53 +0000

    DB backup

--- backup.sh
+++ backup.sh
@@ -8,7 +8,7 @@ db_pass="111"
 backup_dir=$(dirname $(readlink -f "$0"))
 datetime=$(date +'%Y-%m-%dT%H:%M:%S')
 
-find backup_dir -mtime +14 -print
+find $backup_dir -mtime +14 -print
 
 mysqldump -u $db_user -h $db_host -P $db_port --password=$db_pass $backup_db | gzip > $backup_dir/$backup_db--$datetime.sql.gz
 tar -zcvf backup_directory.tar.gz /var/www
